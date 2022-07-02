abaf27716c0e0ff94942880479f8d188344b2aa3
Raphael409 <raphaelsimba15@gmail.com>
2022-07-02 04:53:16 -0700

    C program to print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,13 +9,8 @@
  *   * Return: 0 if exited properly, non-zero otherwise
  *
  *    */
-
 int main(void)
-
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
-
 			return (0);
-
 }
