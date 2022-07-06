3a93a8564ee1f8b520a8d69ed0e07dfe9ab04366
Raphael409 <raphaelsimba15@gmail.com>
2022-07-06 09:50:11 -0700

    10 alphabets in lower case

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * void print_alphabet_x10 - Entry point
+ *
+ * Return: 0 when successful
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i <= 10)
+	{
+		chr = 'a';
+
+		while (chr <= 'z')
+		{
+			_putchar(chr);
+			chr++;
+		}
+		i++;
+		_putchar('\n');
+	}
+}
