4340cfa9d587a35b337d156e5b7b2bd5a6cd6573
Raphael409 <raphaelsimba15@gmail.com>
2022-07-06 10:20:38 -0700

    check lowercase alphabets

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - To check fow lower case
+ *alphabets
+ *
+ * @c: ascii character
+ *
+ * Return: 1 if lowercase 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
