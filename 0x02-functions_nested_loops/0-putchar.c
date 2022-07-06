920980e4fc43187c6cd5cf44e0f2147940bed879
Raphael409 <raphaelsimba15@gmail.com>
2022-07-06 00:44:22 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 when successful
+ */
+int main(void)
+{
+	char txt[9] =  "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(txt[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
