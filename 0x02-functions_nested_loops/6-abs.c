8ada7d8b7f44149528a76ac65eb778df150ce128
Raphael409 <raphaelsimba15@gmail.com>
2022-07-06 11:12:15 -0700

    to write the absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,10 +3,21 @@
 /**
  * _abs - to print the absolute value of an
  * integer
- * @int: integer
- * Return: 0 when success
+ * @n: integer
+ * Return: absolute value
  */
-int _abs(int)
+int _abs(int n)
 {
-	_putchar('%d', abs(int));
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
 }
