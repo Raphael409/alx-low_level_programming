b63ca84fbd8ea70a65a0708c863f37718431de80
Raphael409 <raphaelsimba15@gmail.com>
2022-07-06 12:50:46 -0700

    to display natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_to_98 - to print natural numbers
+ * @n: integer
+ * Return: integer
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n, i <= 98; i++)
+	{
+		if ((i % 1) == 1)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
