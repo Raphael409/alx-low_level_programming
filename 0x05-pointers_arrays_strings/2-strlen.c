71f033e46ece4d3a9c33e0f977f66c5c523f5272
Raphael409 <raphaelsimba15@gmail.com>
2022-07-12 00:36:45 -0700

    Arrays and pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - finds the length of a string
+ * @s: string to determine the length
+ * Return: 0 when success
+ */
+int _strlen(char *s);
+{
+	int n;
+	
+	n = 0;
+	while (s[n] != '\0')
+	{
+		n++;
+	}
+	return (u);
+}
+
